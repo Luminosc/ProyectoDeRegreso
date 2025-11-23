fb7a8525a7f54bec68c3b9fd306427309719c0f4
Luminosc <149198625+Luminosc@users.noreply.github.com>
2025-11-23 17:18:30 -0600

    Correcciones de sección 5

--- Tienda-Online/src/app/listado-productos/listado-productos.component.ts
+++ Tienda-Online/src/app/listado-productos/listado-productos.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { ProductoComponent } from './producto/producto.component';
 import { producto } from './producto/producto.model';
+import { PercentPipe } from '@angular/common';
+import { log } from 'console';
 
 @Component({
   selector: 'app-listado-productos',
@@ -9,15 +11,16 @@ import { producto } from './producto/producto.model';
   styleUrl: './listado-productos.component.css',
 })
 export class ListadoProductosComponent {
-  Producto: producto[]= [
-    new producto('Chamarra',10)
-  ];
-  agregarProducto(ProductoInput: String, PrecioUInput:number)
-  {
-    if (PrecioUInput >= 0 )
-    {
+  Producto: producto[] = [new producto('Chamarra', 10)];
+  agregarProducto(ProductoInput: String, PrecioUInput: number) {
+    if (
+      ProductoInput.trim() === '' ||
+      PrecioUInput == null ||
+      PrecioUInput <= 0
+    ) {
+      console.log('Valor erroneo');
+    } else {
       this.Producto.push(new producto(ProductoInput, PrecioUInput));
     }
-    
   }
 }
